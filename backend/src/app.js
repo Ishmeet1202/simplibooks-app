61ce933957ceca695eb1a4f6d5763123cb729b9f
Ishmeet Singh <ishmeet.singh1202@gmail.com>
2025-11-25 01:02:24 +0530

    Add health check endpoint to root route

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,6 +16,8 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(cookieParser());
 
+app.get("/", (_, res) => res.send("Ok"));
+
 // IMPORTING ROUTERS
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/users.routes.js";
